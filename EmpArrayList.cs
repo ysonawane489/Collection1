b2f3914b3368dc72ba056492005162dc98a0a153
YOGESH <YOGESH@DESKTOP-B3HSVTG>
2021-07-19 08:50:29 +0530

    ArrayListObject

---
+++ EmpArrayList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    class EmpArrayList
+    {
+        public static void Main(string[] args)
+        {
+            List<Employee> lst = new List<Employee>();
+            lst.Add(new Employee() { EmpId = 101, EmpName = "Patil", Address = "Pune" });
+            lst.Add(new Employee() { EmpId = 102, EmpName = "Soanwane", Address = "Nagar" });
+            lst.Add(new Employee() { EmpId = 103, EmpName = "Mhaske", Address = "Nasik" });
+            lst.Add(new Employee() { EmpId = 104, EmpName = "Shinde", Address = "Satara" });
+
+            foreach (Employee v in lst)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    class Employee
+    {
+        private int empId;
+        private string empName;
+        private string address;
+
+        public int EmpId { get => empId; set => empId = value; }
+        public string EmpName { get => empName; set => empName = value; }
+        public string Address { get => address; set => address = value; }
+    }
+}
